8266761dd5e33cdaaf29133ada2f6d9987eb1d89
JavierMendozaBenitez <javier.mendoza.benitez@gmail.com>
2021-09-17 13:07:08 -0300

    Tercera subida

---
+++ Ejercicio25/Ejercicio25/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace Ejercicio25
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBinario = new System.Windows.Forms.TextBox();
+            this.txtDecimal = new System.Windows.Forms.TextBox();
+            this.txtResultadoDec = new System.Windows.Forms.TextBox();
+            this.txtResultadoBin = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBinToDec = new System.Windows.Forms.Button();
+            this.btnDecToBin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBinario
+            // 
+            this.txtBinario.Location = new System.Drawing.Point(122, 60);
+            this.txtBinario.Name = "txtBinario";
+            this.txtBinario.Size = new System.Drawing.Size(100, 23);
+            this.txtBinario.TabIndex = 0;
+            // 
+            // txtDecimal
+            // 
+            this.txtDecimal.Location = new System.Drawing.Point(122, 149);
+            this.txtDecimal.Name = "txtDecimal";
+            this.txtDecimal.Size = new System.Drawing.Size(100, 23);
+            this.txtDecimal.TabIndex = 1;
+            // 
+            // txtResultadoDec
+            // 
+            this.txtResultadoDec.Location = new System.Drawing.Point(309, 60);
+            this.txtResultadoDec.Name = "txtResultadoDec";
+            this.txtResultadoDec.Size = new System.Drawing.Size(100, 23);
+            this.txtResultadoDec.TabIndex = 2;
+            // 
+            // txtResultadoBin
+            // 
+            this.txtResultadoBin.Location = new System.Drawing.Point(309, 149);
+            this.txtResultadoBin.Name = "txtResultadoBin";
+            this.txtResultadoBin.Size = new System.Drawing.Size(100, 23);
+            this.txtResultadoBin.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Binario a Decimal";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Decimal a Binario";
+            // 
+            // btnBinToDec
+            // 
+            this.btnBinToDec.Location = new System.Drawing.Point(228, 60);
+            this.btnBinToDec.Name = "btnBinToDec";
+            this.btnBinToDec.Size = new System.Drawing.Size(75, 23);
+            this.btnBinToDec.TabIndex = 6;
+            this.btnBinToDec.Text = "->";
+            this.btnBinToDec.UseVisualStyleBackColor = true;
+            this.btnBinToDec.Click += new System.EventHandler(this.btnBinToDec_Click);
+            // 
+            // btnDecToBin
+            // 
+            this.btnDecToBin.Location = new System.Drawing.Point(228, 149);
+            this.btnDecToBin.Name = "btnDecToBin";
+            this.btnDecToBin.Size = new System.Drawing.Size(75, 23);
+            this.btnDecToBin.TabIndex = 7;
+            this.btnDecToBin.Text = "->";
+            this.btnDecToBin.UseVisualStyleBackColor = true;
+            this.btnDecToBin.Click += new System.EventHandler(this.btnDecToBin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 237);
+            this.Controls.Add(this.btnDecToBin);
+            this.Controls.Add(this.btnBinToDec);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResultadoBin);
+            this.Controls.Add(this.txtResultadoDec);
+            this.Controls.Add(this.txtDecimal);
+            this.Controls.Add(this.txtBinario);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBinario;
+        private System.Windows.Forms.TextBox txtDecimal;
+        private System.Windows.Forms.TextBox txtResultadoDec;
+        private System.Windows.Forms.TextBox txtResultadoBin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBinToDec;
+        private System.Windows.Forms.Button btnDecToBin;
+    }
+}
+
---
+++ Ejercicio25/Ejercicio25/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades22;
+
+namespace Ejercicio25
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBinToDec_Click(object sender, EventArgs e)
+        {
+            this.txtResultadoDec.Text = ((NumeroDecimal)((NumeroBinario)txtBinario.Text)).Numero.ToString();
+        }
+
+        private void btnDecToBin_Click(object sender, EventArgs e)
+        {
+            double auxIngre;
+
+            if(double.TryParse(this.txtDecimal.Text, out auxIngre))
+            {
+                this.txtResultadoBin.Text = ((NumeroBinario)((NumeroDecimal)auxIngre)).Numero;
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtResultadoDec.Enabled = false;
+            txtResultadoBin.Enabled = false;
+        }
+
+    }
+}
