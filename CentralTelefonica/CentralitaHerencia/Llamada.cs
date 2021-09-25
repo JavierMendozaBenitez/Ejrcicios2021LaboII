b1a33298050e45345886f97a48c02a46860518dc
JavierMendozaBenitez <javier.mendoza.benitez@gmail.com>
2021-09-25 20:49:21 -0300

    25-09-2021

---
+++ CentralTelefonica/CentralitaHerencia/Llamada.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+
+namespace CentralitaHerencia
+{
+    public class Llamada
+    {
+        protected float duracion;
+        protected string nroDestino;
+        protected string nroOrigen;
+        public enum TipoLlamada { Local, Provincial, Todas}
+
+        public Llamada (float duracion, string nroDestino, string nroOrigen)
+        {
+            this.duracion = duracion;
+            this.nroDestino = nroDestino;
+            this.nroOrigen = nroOrigen;
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Duracion: " + Duracion);
+            sb.AppendLine("Nro de destino: " + NroDestino);
+            sb.AppendLine("Nro de origen: " + NroOrigen);
+
+            return sb.ToString();
+        }
+
+        //-1 llamada1 es mayor a llamada2, 1 llamada2 es mayor a llamada1, 0 si son iguales
+        public static int OrdenarPorDuracion(Llamada llamada1, Llamada llamada2)
+        {
+            if(llamada1.Duracion > llamada2.Duracion)
+            {
+                return -1;
+            }
+            else if(llamada1.Duracion < llamada2.Duracion)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+
+        public float Duracion
+        {
+            get
+            {
+                return this.duracion;
+            }
+        }
+
+        public string NroDestino
+        {
+            get
+            {
+                return this.nroDestino;
+            }
+        }
+
+        public string NroOrigen
+        {
+            get
+            {
+                return this.nroOrigen;
+            }
+        }
+
+
+    }
+}
---
+++ CentralTelefonica/CentralitaHerencia/Provincial.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    public class Provincial : Llamada
+    {
+        public enum Franja { Franja_1, Franja_2, Franja_3 }
+
+        protected Franja franjaHoraria;
+
+        public Provincial (Franja miFranja, Llamada llamada)
+        : this(llamada.NroOrigen, miFranja, llamada.Duracion, llamada.NroDestino)
+        { }
+
+        public Provincial (string origen, Franja miFranja, float duracion, string destino)
+            : base (duracion, destino, origen)
+        {
+            this.franjaHoraria = miFranja;
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("Costo de la llamada: " + this.CostoLlamada);
+            sb.AppendLine("Franja horarria: " + this.franjaHoraria);
+
+            return sb.ToString();
+        }
+
+        private float CalcularCosto()
+        {
+            float retorno;
+
+            switch (this.franjaHoraria)
+            {
+                case Franja.Franja_1:
+                    retorno = (float)(base.Duracion * 0.99);
+                    break;
+                case Franja.Franja_2:
+                    retorno = (float)(base.Duracion * 1.25);
+                    break;
+                default:
+                    retorno = (float)(base.Duracion * 0.66);
+                    break;
+            }
+
+            return retorno;
+        }
+
+        public float CostoLlamada
+        {
+            get
+            {
+                return this.CalcularCosto();
+            }
+        }
+
+    }
+}
