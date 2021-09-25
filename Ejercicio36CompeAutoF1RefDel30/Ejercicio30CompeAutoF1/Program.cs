965201a9597c960849a0ceabd70d5a2604bdfc46
JavierMendozaBenitez <javier.mendoza.benitez@gmail.com>
2021-09-25 06:04:54 -0300

    25-09-2021

---
+++ Ejercicio36CompeAutoF1RefDel30/Ejercicio30CompeAutoF1/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Entidades30CompeAutoF1;
+
+namespace Ejercicio30CompeAutoF1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Competencia compeF1 = new Competencia(10, 2, Competencia.TipoCompetencia.F1);
+
+            AutoF1 auto1 = new AutoF1(1, "Ferrari", 1000);
+            AutoF1 auto2 = new AutoF1(2, "Ford", 2000);
+            AutoF1 auto3 = new AutoF1(3, "Renault", 3000);
+
+            //Competencia compeMoto = new Competencia(10, 2, Competencia.TipoCompetencia.MotoCross);
+
+            MotoCross moto1 = new MotoCross(1, "Yamaha", 150);
+            MotoCross moto2 = new MotoCross(2, "Harley", 200);
+            MotoCross moto3 = new MotoCross(3, "Zanella", 300);
+
+
+
+
+
+            //Preguntar si esta en la lista
+            Console.WriteLine("Está en la lista auto1: {0}", compeF1 == auto1);
+
+            //Agregar a la lista
+            Console.WriteLine("Se agrega a la lista auto1: {0}", compeF1 + auto1);
+            //Preguntar si esta en la lista
+            Console.WriteLine("Está en la lista auto1: {0}", compeF1 == auto1);
+
+
+
+            // Preguntar si esta en la lista
+            Console.WriteLine("Está en la lista auto2: {0}", compeF1 == auto2);
+            //Agregar a la lista
+            Console.WriteLine("Se agrega a la lista auto2: {0}", compeF1 + auto2);
+            //Preguntar si esta en la lista
+            Console.WriteLine("Está en la lista auto2: {0}", compeF1 == auto2);
+
+
+
+            // Preguntar si esta en la lista
+            Console.WriteLine("Está en la lista auto3: {0}", compeF1 == auto3);
+            //Agregar a la lista
+            Console.WriteLine("Se agrega a la lista auto3: {0}", compeF1 + auto3);
+            //Preguntar si esta en la lista
+            Console.WriteLine("Está en la lista auto3: {0}", compeF1 == auto3);
+
+
+
+            //Quitar de la lista
+            Console.WriteLine("Quito auto1 :{0}", compeF1 - auto1);
+            Console.WriteLine("Muestro auto1 \n{0}", auto1.MostrarDatos());
+            //Agregar a la lista
+            Console.WriteLine("Se agrega a la lista auto3: {0}", compeF1 + auto3);
+            //muestro la lista sin auto1
+            Console.WriteLine("Muestro lista \n{0}", compeF1.Mostrar());
+            //Preguntar si esta en la lista
+            Console.WriteLine("Está en la lista auto1 con ==: {0}", compeF1 == auto1);
+            //Preguntar si esta en la lista
+            Console.WriteLine("Está en la lista auto1 con !=: {0}", compeF1 != auto1);
+
+            //muestro esto para demostrar que no se cargo
+            Console.WriteLine("Está en la lista moto1: {0}", compeF1 == moto1);
+            //Agregar a la lista
+            Console.WriteLine("Se agrega a la lista moto1: {0}", compeF1 + moto1);
+            //Preguntar si esta en la lista
+            Console.WriteLine("Está en la lista moto1: {0}", compeF1 == moto1);
+            Console.WriteLine("MUESTRO COMPE \n{0}", compeF1.Mostrar());
+
+            /*//Preguntar si esta en la lista
+            Console.WriteLine("Está en la lista moto1: {0}", compeMoto == moto1);
+
+            //Agregar a la lista
+            Console.WriteLine("Se agrega a la lista moto1: {0}", compeMoto + moto1);
+            //Preguntar si esta en la lista
+            Console.WriteLine("Está en la lista moto1: {0}", compeMoto == moto1);
+
+            // Preguntar si esta en la lista
+            Console.WriteLine("Está en la lista moto2: {0}", compeMoto == moto2);
+            //Agregar a la lista
+            Console.WriteLine("Se agrega a la lista moto2: {0}", compeMoto + moto2);
+            //Preguntar si esta en la lista
+            Console.WriteLine("Está en la lista moto2: {0}", compeMoto == moto2);
+
+            // Preguntar si esta en la lista
+            Console.WriteLine("Está en la lista moto3: {0}", compeMoto == moto3);
+            //Agregar a la lista
+            Console.WriteLine("Se agrega a la lista moto3: {0}", compeMoto + moto3);
+            //Preguntar si esta en la lista
+            Console.WriteLine("Está en la lista moto3: {0}", compeMoto == moto3);
+
+            //Quitar de la lista
+            Console.WriteLine("Quito moto1 :{0}", compeMoto - moto1);
+            Console.WriteLine("Muestro moto1 \n{0}", compeMoto.Mostrar());
+            //Agregar a la lista
+            Console.WriteLine("Se agrega a la lista moto3: {0}", compeMoto + moto3);
+            //muestro la lista sin auto1
+            Console.WriteLine("Muestro lista \n{0}", compeMoto.Mostrar());
+            //Preguntar si esta en la lista
+            Console.WriteLine("Está en la lista moto1 con ==: {0}", compeMoto == moto1);
+            //Preguntar si esta en la lista
+            Console.WriteLine("Está en la lista moto1 con !=: {0}", compeMoto != moto1);*/
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio36CompeAutoF1RefDel30/Entidades30CompeAutoF1/AutoF1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+
+namespace Entidades30CompeAutoF1
+{
+    public class AutoF1 : VehiculoDeCarrera
+    {
+        private short caballosDeFuerza;
+
+        public AutoF1(short numero, string escuderia) 
+            : base(numero, escuderia)
+        {
+            base.CantidadCombustible = 0;
+            base.VueltasRestantes = 0;
+        }
+        public AutoF1(short numero, string escuderia, short caballosdeFuerza)
+            : this(numero, escuderia)
+        {
+            this.CaballosDeFuerza = caballosDeFuerza;
+        }
+
+        public short CaballosDeFuerza
+        {
+            get
+            {
+                return this.caballosDeFuerza;
+            }
+            set
+            {
+                this.caballosDeFuerza = value;
+            }
+        }             
+
+        public new string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(base.MostrarDatos());
+            sb.AppendLine("Caballos de fuerza " + this.CaballosDeFuerza);
+
+            return sb.ToString(); 
+        }
+
+        public static bool operator == (AutoF1 a1, AutoF1 a2)
+        {
+            return (VehiculoDeCarrera)a1 == (VehiculoDeCarrera)a2 && a1.CaballosDeFuerza == a2.CaballosDeFuerza;
+        }
+
+        public static bool operator !=(AutoF1 a1, AutoF1 a2)
+        {
+            return !(a1 == a2);
+        }
+    }
+}
