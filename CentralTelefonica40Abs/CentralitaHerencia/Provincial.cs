6baea279969232a2f3278f7a9388320f60f28d1d
JavierMendozaBenitez <javier.mendoza.benitez@gmail.com>
2021-09-27 15:20:10 -0300

    27-09-2021

---
+++ CentralTelefonica40Abs/CentralitaHerencia/Provincial.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    public class Provincial : Llamada
+    {
+        public enum Franja { Franja_1, Franja_2, Franja_3 }
+
+        protected Franja franjaHoraria;
+
+        public Provincial (Franja miFranja, Llamada llamada)
+        : this(llamada.NroOrigen, miFranja, llamada.Duracion, llamada.NroDestino)
+        { }
+
+        public Provincial (string origen, Franja miFranja, float duracion, string destino)
+            : base (duracion, destino, origen)
+        {
+            this.franjaHoraria = miFranja;
+        }
+
+        protected override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("Costo de la llamada: " + this.CostoLlamada);
+            sb.AppendLine("Franja horarria: " + this.franjaHoraria);
+
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return this.Mostrar();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this.GetType() == obj.GetType();
+            //return obj is Provincial;
+        }
+
+        private float CalcularCosto()
+        {
+            float retorno;
+
+            switch (this.franjaHoraria)
+            {
+                case Franja.Franja_1:
+                    retorno = (float)(base.Duracion * 0.99);
+                    break;
+                case Franja.Franja_2:
+                    retorno = (float)(base.Duracion * 1.25);
+                    break;
+                default:
+                    retorno = (float)(base.Duracion * 0.66);
+                    break;
+            }
+
+            return retorno;
+        }
+
+        public override float CostoLlamada
+        {
+            get
+            {
+                return this.CalcularCosto();
+            }
+        }
+
+    }
+}
---
+++ CentralTelefonica40Abs/Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CentralitaHerencia;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mi central
+            Centralita c = new Centralita("Fede Center"); // Mis 4 llamadas
+            Local l1 = new Local("Bernal", 30, "Rosario", 2.65f);
+            Provincial l2 = new Provincial("Morón", Provincial.Franja.Franja_1, 21, "Bernal"); 
+            Local l3 = new Local("Lanús", 45, "San Rafael", 1.99f);
+            Provincial l4 = new Provincial(Provincial.Franja.Franja_3, l2); // Las llamadas se irán registrando en la Centralita.
+            //Provincial l4 = new Provincial("Varela", Provincial.Franja.Franja_2, 23, "Quilmes");                                                                 // La centralita mostrará por pantalla todas las llamadas según las vaya registrando.
+
+            Console.WriteLine("Agrego l1 con el + {0}", c + l1);
+            Console.WriteLine("Agrego l2 con el + {0}", c + l2);
+            Console.WriteLine("Agrego l3 con el + {0}", c + l3);
+            Console.WriteLine("Agrego l4 con el + {0}", c + l4);
+            
+            c.OrdenarLlamadas();
+            Console.WriteLine(c.ToString()); 
+            
+            Console.ReadKey();
+        }
+    }
+}
